31290c90900d6f72f88d2c574cfcda3ced031492
Nirantarjot1 <95531047+Nirantarjot1@users.noreply.github.com>
2023-01-12 12:35:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 img="";
+stats="";
 
 function setup(){
     canvas=createCanvas(640 , 420);
@@ -16,4 +17,27 @@ function draw(){
     noFill();
     stroke("#fc4235");
     rect(30 , 60 , 450 , 350); 
+
+    fill("#3a4abd");
+    text("Cat" , 320 , 120);
+    noFill();
+    stroke("#3a4abd");
+    rect(300 , 90 , 270 , 320);
+}
+
+objectDetector=ml5.objectDetector('cocossd' , modelLoaded);
+document.getElementById("stats").innerHTML="Status  : Detecting Objects ";
+
+
+function modelLoaded(){
+    console.log("Model is Loaded");
+    stats=true;
+    objectDetector.detect(img , gotResult);
+}
+
+function gotResult(error ,  results){
+    if(error){
+        console.log(error);
+    }
+console.log(results);
 }
